22830a1d7c8040603e7f3dc2466c06784d7e6fd9
Bo Victor Thomsen <bo.victor.thomsen@gmail.com>
2022-03-03 05:10:51 +0100

    andeby updata

---
+++ andeby_backup/restore_pg13.cmd
@@ -0,0 +1,7 @@
+@echo on
+set PGDATABASE=fdc_andeby
+set PGUSER=postgres
+set PGHOST=localhost
+set PGPORT=5432
+"C:\Program Files\PostgreSQL\13\bin\pg_restore.exe" --dbname "%PGDATABASE%" --verbose "fdc_andeby.2022_03_02_ 6_55_59.backup"
+pause
