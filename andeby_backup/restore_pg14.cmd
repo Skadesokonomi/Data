87b8877a61f626d76b577f845d9852fa98fab9e5
Bo Victor Thomsen <bo.victor.thomsen@gmail.com>
2022-03-03 05:48:56 +0100

    rettelse

--- andeby_backup/restore_pg14.cmd
+++ andeby_backup/restore_pg14.cmd
@@ -3,5 +3,5 @@ set PGDATABASE=fdc_andeby
 set PGUSER=postgres
 set PGHOST=localhost
 set PGPORT=5432
-"C:\Program Files\PostgreSQL\14\bin\pg_restore.exe" --dbname "%PGDATABASE%" --verbose "fdc_andeby.2022_03_02_ 6_55_59.backup"
+"C:\Program Files\PostgreSQL\14\bin\pg_restore.exe" --dbname "%PGDATABASE%" --verbose "fdc_andeby.2022_03_03_ 5_45_27.backup"
 pause
